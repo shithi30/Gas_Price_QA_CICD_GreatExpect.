829b9fbdd74041077a767a192906bf1907805396
Shithi Maitra <shithi30@gmail.com>
2024-09-05 17:00:11 -0400

    Update scrape_gas_price.py

--- scrape_gas_price.py
+++ scrape_gas_price.py
@@ -1,4 +1,5 @@
-# example_script.py
+
+from datetime import datetime, timedelta
 
 with open("Gas Price Data.txt", "w") as file:
-    file.write("This is a sujar file.")
+    file.write("This is written at " + str(datetime.now()-timedelta(hours=4)))
